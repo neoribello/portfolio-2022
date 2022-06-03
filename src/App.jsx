c106d59ab407c0bb624042c10e9def3a05d68704
neoribello <46947156+neoribello@users.noreply.github.com>
2022-06-03 11:14:56 -0700

    Removed the test jargon

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import PortfolioTheme from './lib/theme/PortfolioTheme';
-import {
-  Typography,
-  Button
-} from "@mui/material";
 
 function App() {
   return (
     <>
     <PortfolioTheme>
       <div className="App">
-        <Typography variant="h4">
-          hello
-        </Typography>
-        <Button
-          variant="resume-button">
-            s
-          </Button>
+       
       </div>
     </PortfolioTheme>
     </>
