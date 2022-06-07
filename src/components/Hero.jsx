0260638d3620559e19aeb8770ce7aeee34ec2f6e
neoribello <46947156+neoribello@users.noreply.github.com>
2022-06-07 16:07:28 -0700

    Implemented the axios call from teh backend

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,43 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { heroData } from '../lib/data';
+import useAxios from '../lib/data';
 
 import { Grid, Box, Typography } from '@mui/material';
 
 
 function Hero() {
   const [data, setData] = useState([]);
-
-  const url = 'http://localhost:1338/api/';
-
+  const [todo, isError, isLoading] = useAxios({
+    url: 'heroes',
+    method: 'get',
+  });
   useEffect(() => {
-    getData();
-  }, [])
+    if(todo && todo.data) setData(todo.data.data[0].attributes)
+ }, [todo]);
 
-  const getData = () => {
-    axios.get(`${url}heroes`)
-      .then((response) => {
-        const sanitizedData = response.data.data[0].attributes;
-        setData(sanitizedData)
-      })
-  }
-  console.log("|||||||||", data)
 
   return (
-    <>
-        <Grid>
+    <>  
+    {isLoading ? (
+      <p>isLoading...</p>
+      ) : (
+        <div>
+          {isError && <p>{isError.message}</p>}
+          <Grid>
           <Box>
             <Typography variant="h1">
-              Neil Oribello
+              {data.heroName}
             </Typography>
             <Typography variant="h2">
-              Web Developer
+              {data.heroHeadline}
             </Typography>
           </Box>
           <Box>
             
           </Box>
-        </Grid>
+            </Grid>
+        </div>
+      )} 
     </>
   );
 }
--- src/lib/data.js
+++ src/lib/data.js
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
 import axios from 'axios';
+const useAxios = (configParams) => {
+    axios.defaults.baseURL = 'http://localhost:1338/api/';
+    const [res, setRes] = useState('');
+    const [err, setErr] = useState('');
+    const [loading, setLoading] = useState(true);
 
-export async function heroData() {
-    return await axios.get("localhost:1338/api/heroes")
+    useEffect(() => {
+       fetchDataUsingAxios(configParams);
+    }, []);
+    
+    const fetchDataUsingAxios = async() => {
+        await axios.request(configParams)
+            .then(res => {
+                setRes(res)
+            })
+            .catch(err => {
+                setErr(err)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    }
+    return [res, err, loading];
 }
+export default useAxios;
