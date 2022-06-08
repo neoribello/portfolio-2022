b2a2cd56257640a79737a90e2d4fc47cdfc8a7e6
neoribello <46947156+neoribello@users.noreply.github.com>
2022-06-08 12:24:58 -0700

    Removed console error and added new axios get call

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,14 +7,14 @@ import { Grid, Box, Typography } from '@mui/material';
 function Hero() {
   const [data, setData] = useState([]);
   const [todo, isError, isLoading] = useAxios({
-    url: 'heroes',
+    url: 'heroes?populate=heroImg',
     method: 'get',
   });
   useEffect(() => {
     if(todo && todo.data) setData(todo.data.data[0].attributes)
  }, [todo]);
-
-
+ console.log(data)
+ 
   return (
     <>  
     {isLoading ? (
@@ -23,21 +23,20 @@ function Hero() {
         <div>
           {isError && <p>{isError.message}</p>}
           <Grid>
-          <Box>
-            <Typography variant="h1">
-              {data.heroName}
-            </Typography>
-            <Typography variant="h2">
-              {data.heroHeadline}
-            </Typography>
-          </Box>
-          <Box>
-
-          </Box>
-          <Box>
+            <Box>
+              <Typography variant="h1">
+                {data.heroName}
+              </Typography>
+              <Typography variant="h2">
+                {data.heroHeadline}
+              </Typography>
+            </Box>
+            <Box
             
-          </Box>
-            </Grid>
+            >
+              
+            </Box>
+          </Grid>
         </div>
       )} 
     </>
